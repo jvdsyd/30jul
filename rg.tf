c9498c2f452bdf700f89e23bd22ffbeba9be04ca
Javed Sayyed <javed@192.168.1.4>
2025-07-30 20:08:04 +0530

    new rg added

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example5" {
   name     = "newrg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example6" {
+  name     = "newrg1"
+  location = "West Europe"
+}
